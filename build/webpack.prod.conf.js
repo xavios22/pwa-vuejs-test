bd14a95a6cdee09566798ab8568a59298d5041a8
xavier <xavier_22_mora@hotmail.com>
2019-04-18 14:49:55 +0200

    on ajout des comandes qui permettent de suavgarder les images pris d'internet dans le cache de l'ordinateur

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -102,7 +102,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'vue-pwa',
       filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      staticFileGlobs: ['dist/**/*.{js,html,css,img}'],
       runtimeCaching: [
         {
           urlPattern: /^http:\/\/res\.cloudinary\.com\//,
